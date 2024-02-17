b5de6197c68b22787812431c3f8d035c8cf57861
Robert Truong <59712697+VinhTruong247@users.noreply.github.com>
2024-02-17 21:48:37 +0700

    Revert "Huy"

--- src/router/AppRoutes.js
+++ src/router/AppRoutes.js
@@ -1,6 +1,4 @@
 import Home from "../components/home/Home";
-import Login from "../pages/Login";
-import SignIn from "../pages/SignIn";
 import Layout from "../layout/Layout";
 
 const AppRoutes = [
@@ -13,15 +11,7 @@ const AppRoutes = [
         element: <Home />,
       },
     ],
-  },
-  {
-    path: "Login",
-    element: <Login />,
-  },
-  {
-    path: "SignIn",
-    element: <SignIn />,
-  },
+  }
 ];
 
 export default AppRoutes;
