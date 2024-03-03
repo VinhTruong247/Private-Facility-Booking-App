958ce605705cf3c410cb3ade2d346b0a585a6b3b
VinhTruong247 <roberttruong247@gmail.com>
2024-03-03 22:13:09 +0700

    Setup Axios & Search Page

---
+++ src/components/search/FilterBar.jsx
@@ -0,0 +1,143 @@
+import {
+  InputLabel,
+  MenuItem,
+  Select,
+  FormControl,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@mui/material";
+import { Accordion } from "react-bootstrap";
+
+const FilterBar = (props) => {
+  const {
+    categoryList,
+    fetchAllCourses,
+    setLoading,
+    setSelectedCategory,
+    selectedCategory,
+    searchValue,
+    setSortOption,
+    sortOption,
+  } = props;
+
+  const handleChangeCategory = (value) => {
+    setLoading(true);
+    setSelectedCategory(value);
+    if (!sortOption) fetchAllCourses(1, value, searchValue);
+    if (sortOption === "createdAt")
+      fetchAllCourses(1, value, searchValue, sortOption);
+    if (sortOption === "high")
+      fetchAllCourses(1, value, searchValue, "price", true);
+    if (sortOption === "low")
+      fetchAllCourses(1, value, searchValue, "price", false);
+  };
+
+  const handleSort = (value) => {
+    setLoading(true);
+    setSortOption(value);
+    if (value === "createdAt")
+      fetchAllCourses(1, selectedCategory, searchValue, value);
+    if (value === "high")
+      fetchAllCourses(1, selectedCategory, searchValue, "price", true);
+    if (value === "low")
+      fetchAllCourses(1, selectedCategory, searchValue, "price", false);
+  };
+
+  return (
+    <>
+      <div className="d-flex align-items-baseline justify-content-between">
+        <div style={{ fontWeight: 500, fontSize: "1.3em" }}>Filters</div>
+
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-standard-label">Sort</InputLabel>
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            label="Age"
+            value={sortOption}
+            onChange={(e) => handleSort(e.target.value)}
+          >
+            {/* <MenuItem value="Most reviewed">Most reviewed</MenuItem>
+            <MenuItem value="Highest Rated">Highest Rated</MenuItem> */}
+            <MenuItem value="createdAt">Newest</MenuItem>
+            <MenuItem value="low">Lowest Price</MenuItem>
+            <MenuItem value="high">Hightest Price</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
+      <hr />
+
+      <Accordion
+        className="filterBar"
+        defaultActiveKey={["0"]}
+        flush
+        alwaysOpen
+      >
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Category</Accordion.Header>
+          <Accordion.Body>
+            <FormControl>
+              <RadioGroup
+                name="controlled-radio-buttons-group"
+                value={selectedCategory}
+                onChange={(e) => handleChangeCategory(e.target.value)}
+              >
+                {categoryList &&
+                  categoryList.length > 0 &&
+                  categoryList.map((item) => {
+                    return (
+                      <FormControlLabel
+                        key={item.id}
+                        control={<Radio value={item.id} />}
+                        label={item.name}
+                      />
+                    );
+                  })}
+              </RadioGroup>
+            </FormControl>
+          </Accordion.Body>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Level</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco
+          </Accordion.Body>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>Language</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco
+          </Accordion.Body>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>Rating</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco
+          </Accordion.Body>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="4">
+          <Accordion.Header>Price</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </>
+  );
+};
+
+export default FilterBar;
