c3f8558f7bea1a8b010ed1fc038fb20b8ab5ff65
VinhTruong247 <roberttruong247@gmail.com>
2024-03-21 00:06:55 +0700

    Add rolesService and fixing wrong api in userService

---
+++ src/services/rolesService.js
@@ -0,0 +1,25 @@
+import axios from "../utils/axiosCustomize"
+
+const postCreateRole = async (data) => {
+    return await axios.post("/roles", data);
+};
+
+const getRoleList = async (params) => {
+    return await axios.get(`/roles`);
+};
+
+
+const getRoleInfo = async (id) => {
+    return await axios.get(`/roles/${id}`);
+};
+
+const deleteRole = async (id) => {
+    return await axios.delete(`/roles/${id}`);
+};
+
+export {
+    postCreateRole,
+    getRoleList,
+    getRoleInfo,
+    deleteRole,
+};
